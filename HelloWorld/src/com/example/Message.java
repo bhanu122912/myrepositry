6415abe4a3b77f3c3e98d2abe6797ebb65a0088b
bmadgam <bmadgam@hydlap128.FTDCORP.NET>
2022-02-07 18:17:39 +0530

    zvafdv

--- HelloWorld/src/com/example/Message.java
+++ HelloWorld/src/com/example/Message.java
@@ -4,7 +4,7 @@ public class Message {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("hasddlslo");
+         System.out.println("hasascddlslo");
 	}
 
 }
